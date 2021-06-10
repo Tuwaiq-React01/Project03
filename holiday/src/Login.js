705d0cacbbebdf7e9625556addff0fc94d8f852c
Batool-Alghamdi <batool.alghamdi@tuwaiq.edu.sa>
2021-06-10 19:49:28 +0300

    styling to do

---
+++ holiday/src/Login.js
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import FacebookLogin from 'react-facebook-login';
+export default function Login() {
+   const [name, setName] = useState('');
+   const [email, setEmail] = useState('');
+   const [picture, setPicture] = useState('');
+   const [token, setToken] = useState('');
+  const responseFacebook = (response) => {
+    if (response.status !== "unknown") {
+      setName(response.name )
+      setEmail( response.email )
+      setPicture(response.picture.data.url )
+      setToken(response.accessToken)
+    }
+    else {
+      console.log("you are not singned in");
+    }
+ }
+     return (
+        <div className="login pt-3">
+        <center>
+        <h1>Login</h1>
+        <br></br>
+    <FacebookLogin
+    appId="135275661915406"
+    autoLoad={false}
+    fields="name,email,picture"
+    callback={responseFacebook} />,
+    {token?
+    <div>
+    <h3>Welcome {name}</h3>
+    <img className="ig" src={picture}></img>
+    <h5>The email {email} is authorized</h5>
+    <br></br>
+    </div>
+    : null}
+      </center>
+      <br></br>
+      </div>
+    )
+}
--- holiday/src/index.js
+++ holiday/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import CA from "./CA"
+import Login from './Login'
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router, Route , Link , Switch} from "react-router-dom";
 import "./App.css"
@@ -15,10 +16,14 @@ ReactDOM.render(
       <li>
         <Link to="/CA">CA</Link>
       </li>
+      <li>
+        <Link to="/Login">Login</Link>
+      </li>
     </ul>
     <Switch>
       <Route exact path="/" component={App} />
       <Route path="/CA" component={CA} />
+      <Route path="/Login" component={Login} />
       {/* <Route component={PageNotFound} /> */}
     </Switch>
   </Router>,
